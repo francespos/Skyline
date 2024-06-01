77f346109ebde9b8efa2f48c6b684beccf0bb703
Francesco Esposito <123214645+francespos@users.noreply.github.com>
2024-06-01 20:55:22 +0200

    Update SkMatrix2.h

--- Includes/Sk/Math/SkMatrix2.h
+++ Includes/Sk/Math/SkMatrix2.h
@@ -1,10 +1,13 @@
 #pragma once 
 
 #include <Sk/Math/SkVector2.h>
+#include <stdbool.h>
 
 typedef struct {
-    float v[4];
+    float v[2][2];
 } SkMatrix2;
+
+SK_API bool 
 
 /// @brief Adds two matrices.
 SK_API void skAdd(SkMatrix2 *m, const SkMatrix2 *m1, const SkMatrix2 *m2);
@@ -16,10 +19,16 @@ SK_API void skSubtract(SkMatrix2 *m, const SkMatrix2 *m1, const SkMatrix2 *m2);
 SK_API void skMultiply(SkMatrix2 *m, float k, const SkMatrix2 *p);
 
 /// @brief Multiplies a matrix and a vector.
-SK_API void skMultiply(SVector2 *v, const SkMatrix2 *p, const SkVector2* w);
+SK_API void skMultiply(SkVector2 *v, const SkMatrix2 *p, const SkVector2* w);
 
 /// @brief Multiplies two matrices.
 SK_API void skMultiply(SkMatrix2 *m, const SkMatrix2 *m1, const SkMatrix2 *m2);
+
+/// @brief Computes the product of a matrix and a vector.
+SK_API SkMatrix2 skGetProduct(const SkMatrix2 *m, const SkVector2 *v);
+
+/// @brief Computes the product of two matrices.
+SK_API SkVector2 skGetProduct(const SkMatrix2 *m1, const SkMatrix2 *m2);
 
 /// @brief Computes matrix's determinant
 SK_API float skGetDeterminant(SkMatrix2 *m);
