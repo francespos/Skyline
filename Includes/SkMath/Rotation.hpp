0a80a1fdd4623fdf64bbbfe8ec01a343b1cf8045
Francesco Esposito <123214645+francespos@users.noreply.github.com>
2024-06-02 20:24:22 +0200

    Update Rotation.hpp

--- Includes/SkMath/Rotation.hpp
+++ Includes/SkMath/Rotation.hpp
@@ -1,4 +1,7 @@
 namespace Sk {
+    class Matrix3;
+    class Translation;
+
     class Rotation {
     public:
         explicit Rotation(float angle);
@@ -7,5 +10,6 @@ namespace Sk {
         float c, s;
     };
 
+    Matrix3 operator*(const Rotation& r, const Translation& t);
     Rotation invert(const Rotation& r);
 }
