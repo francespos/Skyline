aab99166e6e777107b087e724f66333bcc579436
Francesco Esposito <123214645+francespos@users.noreply.github.com>
2024-06-02 19:42:09 +0200

    Update Vector2.hpp

--- Includes/SkMath/Vector2.hpp
+++ Includes/SkMath/Vector2.hpp
@@ -19,15 +19,8 @@ private:
     float v[2];
 };
 
-/// @brief Computes dot product of two vectors.
 float getDotProduct(const Vector2& v1, const Vector2& v2);
-
-/// @brief Computes vector's length.
 float getLength(const Vector2& v);
-
-/// @brief Computes vector's squared length.
 float getLength2(const Vector2& v);
-
-/// @brief Normalizes a vector.
 Vector2 normalize(const Vector2& v);
 } // namespace Sk
