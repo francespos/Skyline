227705e0a004cff4b70bf7af3543fd8b47ed8b3d
Francesco Esposito <123214645+francespos@users.noreply.github.com>
2024-06-02 20:04:10 +0200

    Update Vector3.hpp

--- Includes/SkMath/Vector3.hpp
+++ Includes/SkMath/Vector3.hpp
@@ -1,32 +1,43 @@
 #pragma once
 
 namespace Sk {
-class Vector2;
+    class Vector2;
+    
+    class Vector3 {
+    public:
+        Vector3(float x, float y, float z);
+    
+        float operator()(unsigned short i) const;
+        float& operator()(unsigned short i);
+    
+        Vector3& operator+=(const Vector3& rhs);
+        Vector3& operator-=(const Vector3& rhs);
+    
+        Vector3& operator*=(float rhs);
+        Vector3& operator/=(float rhs);
+    
+        Vector3& normalize();
+    private:
+        float v[3];
+    };
 
-class Vector3 {
-public:
-    Vector3(float x, float y, float z);
+    bool operator==(const Vector3& lhs, const Vector3& rhs);
+    bool operator!=(const Vector3& lhs, const Vector3& rhs);
 
-    float operator()(unsigned short i) const;
-    float& operator()(unsigned short i);
+    Vector3 operator+(const Vector3& lhs, const Vector3& rhs);
+    Vector3 operator-(const Vector3& lhs, const Vector3& rhs);
 
-    Vector3& operator+=(const Vector3& rhs);
-    Vector3& operator-=(const Vector3& rhs);
+    Vector3 operator*(float lhs, const Vector3& rhs);
+    Vector3 operator*(const Vector3& lhs, float rhs);
 
-    Vector3& operator*=(float rhs);
-    Vector3& operator/=(float rhs);
-
-    Vector3& normalize();
-private:
-    float v[3];
-};
-
-float getDotProduct(const Vector3& v1, const Vector3& v2);
-Vector3 getCrossProduct(const Vector3& v1, const Vector3& v2);
-
-float getLength(const Vector3& v);
-float getLength2(const Vector3& v);
-
-Vector3 normalize(const Vector3& v);
-Vector3 convertToHomogeneousCoordinates(const Vector2& v);
-} // namespace Sk
+    Vector3 operator/(const Vector3& lhs, float rhs);
+    
+    float getDotProduct(const Vector3& v1, const Vector3& v2);
+    Vector3 getCrossProduct(const Vector3& v1, const Vector3& v2);
+    
+    float getLength(const Vector3& v);
+    float getLength2(const Vector3& v);
+    
+    Vector3 normalize(const Vector3& v);
+    Vector3 convertToHomogeneousCoordinates(const Vector2& v);
+}
