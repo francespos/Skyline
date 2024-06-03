e3d9c52fb7450135b724402e455fcfb6404dc7a9
Francesco Esposito <123214645+francespos@users.noreply.github.com>
2024-06-03 08:43:52 +0200

    Create Rotation.cpp

---
+++ Source/Rotation.cpp
@@ -0,0 +1,19 @@
+#include <SkMath/Rotation.hpp>
+#include <cmath>
+#include <SkMath/Matrix3.hpp>
+
+namespace Sk {
+    Rotation::Rotation(float angle): s{sinf(angle)}, c{cosf(angle)} {}
+
+    float Rotation::getAngle() const { return atanf(s / c); }
+
+    Rotation& Rotation::invert() {
+        *this = invert(*this);
+        return *this;
+    }
+
+    Matrix3 operator*(const Rotation& r, const Translation& t) {
+        Matrix3 ret{r, t};
+        return ret;
+    }
+}
