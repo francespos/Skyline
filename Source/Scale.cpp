28eb159abc0a237eecd118b7d7d429c1405a29d1
Francesco Esposito <123214645+francespos@users.noreply.github.com>
2024-06-03 08:49:31 +0200

    Create Scale.cpp

---
+++ Source/Scale.cpp
@@ -0,0 +1,13 @@
+namespace Sk {
+    Scale::Scale(float x, float y) : x{x}, y{y} {}
+
+    Scale& Scale::invert() {
+        *this = invert(this);
+        return *this;
+    }
+
+    Scale invert(const Scale& s) {
+        Scale ret{1.0f / s.x, 1.0f / s.y};
+        return ret;
+    }
+}
