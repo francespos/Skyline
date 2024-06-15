5aa14a5487cf871956c6eeea0ea2e4e393ff1990
Francesco Esposito <123214645+francespos@users.noreply.github.com>
2024-06-15 18:09:02 +0200

    Update Matrix.hpp

--- Include/Sk/Math/Matrix.hpp
+++ Include/Sk/Math/Matrix.hpp
@@ -3,155 +3,151 @@
 #include <Sk/Math/Vector.hpp>
 
 namespace Sk {
-    template<Numeric T, unsigned short M, unsigned short N> struct Matrix {
-        std::array<T, M * N> v;
+    template<unsigned short N> struct Matrix {
+        std::array<float, N * N> v;
     
-        T operator()(unsigned short r, unsigned short c) const;
-        T& operator()(unsigned short r, unsigned short c);
+        float operator()(unsigned short r, unsigned short c) const;
+        float& operator()(unsigned short r, unsigned short c);
     
         Matrix& operator+=(const Matrix& rhs);
         Matrix& operator-=(const Matrix& rhs);
     
-        Matrix& operator*=(T rhs);
-
-        template<unsigned short P>
-        Matrix<T, M, P>& operator*=(const Matrix<T, N, P>& rhs);
+        Matrix& operator*=(float rhs);
+        Matrix& operator*=(const Matrix& rhs);
     
-        Matrix& operator/=(T rhs);
+        Matrix& operator/=(float rhs);
     };
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    bool operator==(const Matrix<T, M, N>& lhs, const Matrix<T, M, N>& rhs);
+    using Matrix2 = Matrix<2>;
+    using Matrix3 = Matrix<3>;
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    bool operator!=(const Matrix<T, M, N>& lhs, const Matrix<T, M, N>& rhs); 
+    template<unsigned short N>
+    bool operator==(const Matrix<N>& lhs, const Matrix<N>& rhs);
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix<T, M, N> operator+(Matrix<T, M, N> lhs, const Matrix<T, M, N>& rhs);
+    template<unsigned short N>
+    bool operator!=(const Matrix<N>& lhs, const Matrix<N>& rhs); 
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix<T, M, N> operator-(Matrix<T, M, N> lhs, const Matrix<T, M, N>& rhs);
+    template<unsigned short N>
+    Matrix operator+(const Matrix<N>& lhs, const Matrix<N>& rhs);
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix<T, M, N> operator*(T lhs, Matrix<T, M, N> rhs);
+    template<unsigned short N>
+    Matrix<N> operator-(const Matrix<N>& lhs, const Matrix<N>& rhs);
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix<T, M, N> operator*(Matrix<T, M, N> lhs, T rhs);
+    template<unsigned short N>
+    Matrix<N> operator*(const Matrix<N>& lhs, float rhs);
 
-    template<Numeric T, unsigned short M, unsigned short N, unsigned short P>
-    Matrix<T, M, P> operator*(const Matrix<T, M, N>& lhs, const Matrix<T, N, P>& rhs);
+    template<unsigned short N>
+    Matrix<N> operator*(float lhs, const Matrix<N>& rhs);
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Vector<T, M> operator*(const Matrix<T, M, N>& lhs, const Vector<T, N>& rhs);
+    template<unsigned short N>
+    Matrix<N> operator*(const Matrix<N>& lhs, const Matrix<N>& rhs);
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix<T, M, N> operator/(const Matrix<T, M, N>& lhs, T rhs);
+    template<unsigned short N>
+    Vector<N> operator*(const Matrix<N>& lhs, const Vector<N>& rhs);
 
-    template<Numeric T>
-    T getDeterminant(const Matrix<T, 2, 2>& m);
+    template<unsigned short N>
+    Matrix<N> operator/(const Matrix<N>& lhs, float rhs);
 
-    template<Numeric T>
-    T getDeterminant(const Matrix<T, 3, 3>& m);
+    float getDeterminant(const Matrix2& m);
+    float getDeterminant(const Matrix3& m);
 
-    template<Numeric T>
-    Matrix<T, 2, 2> invert(const Matrix<T, 2, 2>& m);
+    Matrix2 invert(const Matrix2& m);
+    Matrix3 invert(const Matrix3& m);
 
-    template<Numeric T>
-    Matrix<T, 3, 3> invert(const Matrix<T, 3, 3>& m);
-
-    template<Numeric T, unsigned short M, unsigned short N>
-    T Matrix<T, M, N>::operator()(unsigned short r, unsigned short c) const {
+    template<unsigned short N>
+    float Matrix<N>::operator()(unsigned short r, unsigned short c) const {
         return v[r * N + c];
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    T& Matrix<T, M, N>::operator()(unsigned short r, unsigned short c) {
+    template<unsigned short N>
+    float& Matrix<N>::operator()(unsigned short r, unsigned short c) {
         return v[r * N + c];
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix& Matrix<T, M, N>::operator+=(const Matrix& rhs) {
-        for (unsigned short i = 0; i < M * N; ++i) {
-            v[i] += rhs.v[i];
-        }
-
+    template<unsigned short N>
+    Matrix& Matrix<N>::operator+=(const Matrix& rhs) {
+        *this = *this + rhs;
         return *this;
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix& Matrix<T, M, N>::operator-=(const Matrix& rhs) {
-        for (unsigned short i = 0; i < M * N; ++i) {
-            v[i] -= rhs.v[i];
-        }
-
+    template<unsigned short N>
+    Matrix& Matrix<N>::operator-=(const Matrix& rhs) {
+        *this = *this - rhs;
         return *this;
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix& Matrix<T, M, N>::operator*=(T rhs) {
-        for (unsigned short i = 0; i < M * N; ++i) {
-            v[i] *= rhs;
-        }
-
-        return *this;
-    }
-
-    template<Numeric T, unsigned short M, unsigned short N>
-    template<unsigned short P>
-    Matrix<T, M, P>& Matrix<T, M, N>::operator*=(const Matrix<T, N, P>& rhs) {
+    template<unsigned short N>
+    Matrix& Matrix<N>::operator*=(float rhs) {
         *this = *this * rhs;
         return *this;
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix& Matrix<T, M, N>::operator/=(T rhs) {
-        for (unsigned short i = 0; i < M * N; ++i) {
-            v[i] /= rhs;
-        }
-
+    template<unsigned short N>
+    Matrix<N>& Matrix<N>::operator*=(const Matrix<N>& rhs) {
+        *this = *this * rhs;
         return *this;
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    bool operator==(const Matrix<T, M, N>& lhs, const Matrix<T, M, N>& rhs) {
+    template<unsigned short N>
+    Matrix& Matrix<N>::operator/=(float rhs) {
+        *this = *this / rhs;
+        return *this;
+    }
+
+    template<unsigned short N>
+    bool operator==(const Matrix<N>& lhs, const Matrix<N>& rhs) {
         return lhs.v == rhs.v;
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    bool operator!=(const Matrix<T, M, N>& lhs, const Matrix<T, M, N>& rhs) {
+    template<unsigned short N>
+    bool operator!=(const Matrix<N>& lhs, const Matrix<N>& rhs) {
         return lhs.v != rhs.v;
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix<T, M, N> operator+(Matrix<T, M, N> lhs, const Matrix<T, M, N>& rhs) {
-        lhs += rhs;
-        return lhs;
+    template<unsigned short N>
+    Matrix<N> operator+(const Matrix<N>& lhs, const Matrix<N>& rhs) {
+        Matrix<N> ret{lhs};
+        
+        for (unsigned int i = 0; i < N * N; ++i) {
+            ret.v[i] += rhs.v[i];
+        }
+
+        return ret;
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix<T, M, N> operator-(Matrix<T, M, N> lhs, const Matrix<T, M, N>& rhs) {
-        lhs -= rhs;
-        return lhs;
+    template<unsigned short N>
+    Matrix<N> operator-(const Matrix<N>& lhs, const Matrix<N>& rhs) {
+        Matrix<N> ret{lhs};
+        
+        for (unsigned int i = 0; i < N * N; ++i) {
+            ret.v[i] -= rhs.v[i];
+        }
+
+        return ret;
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix<T, M, N> operator*(T lhs, Matrix<T, M, N> rhs) {
-        rhs *= lhs;
-        return rhs;
+    template<unsigned short N>
+    Matrix<N> operator*(const Matrix<N>& lhs, float rhs) {
+        Matrix<N> ret{lhs};
+        
+        for (unsigned int i = 0; i < N * N; ++i) {
+            ret.v[i] *= rhs;
+        }
+
+        return ret;
+    }
+        
+    template<unsigned short N>
+    Matrix<N> operator*(float lhs, const Matrix<N>& rhs) {
+        return rhs * lhs;
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix<T, M, N> operator*(Matrix<T, M, N> lhs, T rhs) {
-        lhs *= rhs;
-        return lhs;
-    ]
+    template<unsigned short N>
+    Matrix<N> operator*(const Matrix<N>& lhs, const Matrix<N>& rhs) {
+        Matrix<N> ret{};
 
-    template<Numeric T, unsigned short M, unsigned short N, unsigned short P>
-    Matrix<T, M, P> operator*(const Matrix<T, M, N>& lhs, const Matrix<T, N, P>& rhs) {
-        Matrix<T, M, P> ret{};
-
-        for (unsigned short i = 0; i < M; ++i) {
-            for (unsigned short j = 0; j < P; ++j) {
+        for (unsigned short i = 0; i < N; ++i) {
+            for (unsigned short j = 0; j < N; ++j) {
                 for (unsigned short k = 0; k < N; ++k) {
                     ret(i, j) += lhs(i, k) * rhs(k, j);
                 }
@@ -159,14 +155,13 @@ namespace Sk {
         }
 
         return ret;
-
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Vector<T, M> operator*(const Matrix<T, M, N>& lhs, const Vector<T, N>& rhs) {
-        Vector<T, M> ret{};
+    template<unsigned short N>
+    Vector<N> operator*(const Matrix<N>& lhs, const Vector<N>& rhs) {
+        Vector<N> ret{};
 
-        for (unsigned short i = 0; i < M; ++i) {
+        for (unsigned short i = 0; i < N; ++i) {
             for (unsigned short j = 0; j < N; ++j) {
                 ret(i) += lhs(i, j) * rhs(j);
             }
@@ -175,33 +170,34 @@ namespace Sk {
         return ret;
     }
 
-    template<Numeric T, unsigned short M, unsigned short N>
-    Matrix<T, M, N> operator/(const Matrix<T, M, N>& lhs, T rhs) {
-        lhs /= rhs;
-        return lhs;
+    template<unsigned short N>
+    Matrix<N> operator/(const Matrix<N>& lhs, float rhs) {
+        Matrix<N> ret{lhs};
+        
+        for (unsigned int i = 0; i < N * N; ++i) {
+            ret.v[i] /= rhs;
+        }
+
+        return ret;
     }
 
-    template<Numeric T>
-    T getDeterminant(const Matrix<T, 2, 2>& m) {
+    float getDeterminant(const Matrix2& m) {
         return m(0, 0) * m(1, 1) - m(0, 1) * m(1, 0);
     }
 
-    template<Numeric T>
-    T getDeterminant(const Matrix<T, 3, 3>& m) {
+    float getDeterminant(const Matrix3& m) {
         return m(0, 0) * (m(1, 1) * m(2, 2) - m(1, 2) * m(2, 1)) - 
             m(0, 1) * (m(1, 0) * m(2, 2) - m(1, 2) * m(2, 0)) + 
             m(0, 2) * (m(1, 0) * m(2, 1) - m(1, 1) * m(2, 0));
     }
 
-    template<Numeric T>
-    Matrix<T, 2, 2> invert(const Matrix<T, 2, 2>& m) {
-        Matrix<T, 2, 2> ret{m(1, 1), -m(1, 0), -m(0, 1), m(0, 0)};
+    Matrix2 invert(const Matrix2& m) {
+        Matrix2 ret{m(1, 1), -m(1, 0), -m(0, 1), m(0, 0)};
         ret /= getDeterminant(m);
         return ret;
     }
 
-    template<Numeric T>
-    Matrix<T, 3, 3> invert(const Matrix<T, 3, 3>& m) {
+    Matrix3 invert(const Matrix3& m) {
         Matrix<T, 3, 3> ret{m(1, 1) * m(2, 2) - m(1, 2) * m(2, 1),
             m(0, 2) * m(2, 1) - m(0, 1) * m(2, 2),
             m(0, 1) * m(1, 2) - m(0, 2) * m(1, 1), 
